41638f3c52f59a0ecbee73fec602ca32a6c3ab13
tamhid-mahmod <tamhidmahmod94@gmail.com>
2025-02-10 13:59:55 +0600

    setuped trpc with @upstash/redis limit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,18 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { HydrateClient, trpc } from "@/trpc/server";
+
 // ----------------------------------------------------------------------
 
-export default function Page() {
-  return <div>I will load videos in the future!</div>;
+export default async function Page() {
+  void trpc.hello.prefetch({ text: "Tamhid" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>asd</ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import { TRPCProvider } from "@/trpc/client";
+
 // ----------------------------------------------------------------------
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,7 +24,9 @@ export default function RootLayout({
   return (
     <ClerkProvider afterSignOutUrl="/">
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <TRPCProvider>{children}</TRPCProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
